3f408ade37a527439d8f003058f5e4ead71ce833
Stefane Fermigier <sf@fermigier.com>
2019-08-02 18:51:27 +0200

    Format w/ prettier

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,80 +4,82 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
+const tailwind = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
 
-const postcssPlugins = [
-  tailwind(),
-]
+const postcssPlugins = [tailwind()];
 
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
 
 module.exports = {
-  siteName: 'Gridsome Portfolio Starter',
-  siteDescription: 'A simple portfolio theme for Gridsome powered by Tailwind CSS v1.0',
-  siteUrl: 'https://gridsome-portfolio-starter.netlify.com',
+  siteName: "Gridsome Portfolio Starter",
+  siteDescription:
+    "A simple portfolio theme for Gridsome powered by Tailwind CSS v1.0",
+  siteUrl: "https://gridsome-portfolio-starter.netlify.com",
   plugins: [
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'blog/**/*.md',
-        typeName: 'Post',
+        path: "blog/**/*.md",
+        typeName: "Post",
         refs: {
           tags: {
-            typeName: 'Tag',
-            route: 'tag/:id',
+            typeName: "Tag",
+            route: "tag/:id",
             create: true
           }
         },
         remark: {
           plugins: [
-            [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
+            [
+              "gridsome-plugin-remark-shiki",
+              { theme: "Material-Theme-Palenight", skipInline: true }
+            ]
           ]
         }
       }
     },
     {
-      use: 'gridsome-plugin-rss',
+      use: "gridsome-plugin-rss",
       options: {
-        contentTypeName: 'Post',
+        contentTypeName: "Post",
         feedOptions: {
-          title: 'Gridsome Portfolio Starter Blog',
-          feed_url: 'https://gridsome-portfolio-starter.netlify.com/rss.xml',
-          site_url: 'https://gridsome-portfolio-starter.netlify.com/'
+          title: "Gridsome Portfolio Starter Blog",
+          feed_url: "https://gridsome-portfolio-starter.netlify.com/rss.xml",
+          site_url: "https://gridsome-portfolio-starter.netlify.com/"
         },
         feedItemOptions: node => ({
           title: node.title,
           description: node.summary,
-          url: 'https://gridsome-portfolio-starter.netlify.com' + node.path,
-          author: 'Andre Madarang',
+          url: "https://gridsome-portfolio-starter.netlify.com" + node.path,
+          author: "Andre Madarang",
           date: node.date
         }),
         output: {
-          dir: './static',
-          name: 'rss.xml'
+          dir: "./static",
+          name: "rss.xml"
         }
       }
     },
     {
-      use: '@gridsome/plugin-sitemap',
+      use: "@gridsome/plugin-sitemap",
       options: {
-        cacheTime: 600000, // default
+        cacheTime: 600000 // default
       }
-    },
+    }
   ],
   transformers: {
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link"
     }
   },
   css: {
     loaderOptions: {
       postcss: {
-        plugins: postcssPlugins,
-      },
-    },
-  },
-}
+        plugins: postcssPlugins
+      }
+    }
+  }
+};
--- src/components/SearchFocus.vue
+++ src/components/SearchFocus.vue
@@ -6,18 +6,18 @@
 export default {
   mounted() {
     this.handler = event => {
-      this.$emit('keyup', event)
-    }
-    window.addEventListener('keyup', this.handler)
+      this.$emit("keyup", event);
+    };
+    window.addEventListener("keyup", this.handler);
   },
   beforeDestroy() {
-    window.removeEventListener('keyup', this.handler)
+    window.removeEventListener("keyup", this.handler);
   }
-}
+};
 </script>
 
 <style scoped>
-  div {
-    display: none;
-  }
+div {
+  display: none;
+}
 </style>
